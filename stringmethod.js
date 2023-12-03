440c567496966a3a491a0a5414d8895f387f477b
sanjaybharat <bharatsahane200@gmail.com>
2023-12-03 20:39:15 +0530

    bharat

---
+++ stringmethod.js
@@ -0,0 +1,9 @@
+let test = " bharat sahane    ";
+console.log(test.length);
+
+let text ="i like mango";
+console.log(text1.silce(1,6)); // start indcluding end ind: exclusive
+
+console.log (text1.slice(1));
+
+console.log(text.slice(-6,-1));

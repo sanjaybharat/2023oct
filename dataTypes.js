a4d16fff586350529a311e7223619e2e6f2ff5a7
sanjaybharat <bharatsahane200@gmail.com>
2023-12-03 21:07:43 +0530

    bharat

---
+++ dataTypes.js
@@ -0,0 +1,77 @@
+// Data types 1) primitive
+// the primitive date types ; single value holder --- in java script include number 
+// number
+//string 
+// boolean 
+// undefined 
+// null
+// symbol 
+// data types 2) non primitive 
+// object
+ 
+
+
+
+// number : whole no or decimal no 
+let amount = 7219800694;
+
+
+// tyeof(): is used to get data type of data
+
+console.log(typeof(amount));
+
+// string  dt : ''/"" 
+
+let city= 'mumbai is big city ';
+console.log(typeof(city));
+
+// boolean : true/ false 
+
+let isfavmovie = false //" true"; // string
+console.log(tyepeof( isfavmovie));
+
+let a ;
+console.log(typeof(a));
+let  b = undefined;
+console.log(b);
+console.log(typeof(b));
+// var false ="bharat " // we can not declare reserved keywords as a variable name 
+
+let r a="none " // string 
+
+let c = null ;
+console.log('c',typeof(c)); 
+document.write(c,)
+
+
+let cash = BigInt("999859845632156") // 1.036656 //1.25
+document.write(cash)
+var stdname = "pooja, om "
+
+// array:[]: listing >> hetrogenous array
+
+let =colors=['red','orange','black']; //homgeous arr
+var studentName = [("name", "om ", 400, true,"pravin","jiya", test ())] //Hetro
+document.write("colors" >>>>, colors) 
+document.write("studentnames  >>", studentName)
+console.log(studentName);
+
+
+function test(){
+    return 50*20;
+     
+}
+var studentName =["pooja  " ,"om ", 400, true ," pravin ","jiya",test ()] //hetro 
+let colors2=[ 'red', 'orange', 'black',"yellow"];
+document.write("<br>",colors2[6])
+
+let cars = []; //empty array
+console.log("cars>>> ,cars");
+
+// length operator : gives length of array or string 
+
+console.log(colors2.length);
+
+
+
+
